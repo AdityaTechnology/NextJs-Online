48cacec5603a74303ba660cfa0994cde5f0260a4
nextjs <nextjs@gmail.com>
2023-10-26 17:18:04 +0530

    First Day

---
+++ my-project/src/app/components/Demo.jsx
@@ -0,0 +1,8 @@
+export default function Demo() {
+	return (
+		<div>
+			<p>-------------------</p>
+			<p>It is Demo Content</p>
+		</div>
+	);
+}
---
+++ my-project/src/app/page.js
@@ -0,0 +1,10 @@
+import Demo from "./components/Demo";
+
+export default function Home() {
+	return (
+		<div className='text-4xl font-bold min-h-screen'>
+			Hello Next JS
+			<Demo />
+		</div>
+	);
+}
