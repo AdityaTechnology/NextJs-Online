572c86c560a23fb7cc3465d7dac969f8238c7d12
nm <nm@gmail.com>
2023-10-30 14:34:18 +0530

    Component creation with Props

--- my-project/src/app/components/ServiceFirst.jsx
+++ my-project/src/app/components/ServiceFirst.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+export default function ServiceFirst(props) {
+	console.log(props);
+	// Props are readonly
+	// props.sname = "new service";
 
-function ServiceFirst() {
+	// destructuring
+	const { sname, fee, address } = props;
+	console.log("Sname = ", sname, " Fee = ", fee);
 	return (
-		<div className='min-h-[300px] bg-cyan-600 text-gray-50 justify-center flex  items-center text-4xl font-bold'>
-			Second Srvice
-		</div>
+		<>
+			<div className='border border-gray-400 p-10'>
+				{/* Second Service :{props.sname}
+				<p>Traing Fee is {props.fee}</p> */}
+				Second Service :{sname}
+				<p>Traing Fee is {fee}</p>
+				{/* <p>Location : {props.address.city}</p> */}
+				<p>Location :: {address.city}</p>
+			</div>
+		</>
 	);
 }
 
-export default ServiceFirst;
+// javascript ( ES1,ES2...ES5,ES6...ES2015,....ES2022)
+
+export function Games() {
+	return (
+		<>
+			<h1>Games Section</h1>
+		</>
+	);
+}
+
+export function Transport() {
+	return (
+		<>
+			<h1>Transportation</h1>
+		</>
+	);
+}
--- my-project/src/app/services/page.jsx
+++ my-project/src/app/services/page.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import ServiceFirst from "../components/ServiceFirst";
+import ServiceFirst, { Games, Transport } from "../components/ServiceFirst";
 import ServiceSecond from "../components/ServiceSecond";
 
 export default function ServicePage() {
+	const location = {
+		city: "delhi",
+		pin: "223344",
+	};
 	return (
-		<div className='min-h-screen max-w-4xl mx-auto mt-20'>
-			<ServiceFirst />
-			<ServiceSecond />
-		</div>
+		<>
+			<Transport />
+			<Games />
+			<div className='min-h-screen max-w-4xl mx-auto mt-20'>
+				<ServiceFirst
+					sname='football'
+					fee={10000}
+					address={location}
+				/>
+				<ServiceFirst
+					sname='cricket'
+					fee={20000}
+					address={location}
+				/>
+				<ServiceFirst
+					sname='hockey'
+					fee={15000}
+					address={location}
+				/>
+			</div>
+		</>
 	);
 }
